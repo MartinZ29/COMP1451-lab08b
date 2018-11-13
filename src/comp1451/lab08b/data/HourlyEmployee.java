e3511c01862d13d57e4758bafef5325d366daa34
Martin Zhang <mtnz6748@gmail.com>
2018-11-12 18:24:07 -0800

    fix HOURLY_WAGE to hourlyWage, and correct the class

--- src/comp1451/lab08b/data/HourlyEmployee.java
+++ src/comp1451/lab08b/data/HourlyEmployee.java
@@ -9,9 +9,10 @@ public class HourlyEmployee extends Employee{
 		super(); 
 	}
 	
-	public HourlyEmployee(String firstName, String lastName, int hoursWorked) {
+	public HourlyEmployee(String firstName, String lastName, int hoursWorked, double hourlyWage) {
 		super(firstName, lastName);
 		this.hoursWorked = hoursWorked;
+		this.hourlyWage = hourlyWage;
 	}
 	
 	public int getHoursWorked() {
@@ -22,12 +23,21 @@ public class HourlyEmployee extends Employee{
 		this.hoursWorked = hoursWorked;
 	}
 	
+	
+	public double getHourlyWage() {
+		return hourlyWage;
+	}
+
+	public void setHourlyWage(double hourlyWage) {
+		this.hourlyWage = hourlyWage;
+	}
+
 	@Override
 	public String toString() {
-		return "HourlyEmployee [hoursWorked=" + hoursWorked + ", HOURLY_WAGE=" + HOURLY_WAGE + "]";
+		return "HourlyEmployee [hoursWorked=" + hoursWorked + ", hourlyWage=" + hourlyWage + "]";
 	}
 	
 	public double calculateMonthlyEarnings(){
-		return hoursWorked*HOURLY_WAGE;
+		return hoursWorked*hourlyWage;
 	}
 }
