271325fbac428c194646b644bd1d339c83878b5a
Martin Zhang <mtnz6748@gmail.com>
2018-11-12 18:26:34 -0800

    add Javadoc

--- src/comp1451/lab08b/driver/CompanyDriver.java
+++ src/comp1451/lab08b/driver/CompanyDriver.java
@@ -1,9 +1,16 @@
+/**
+ * Lab 08b for COMP 1451 @BCIT
+ * 
+ * @author Martin
+ * @version Nov 12, 2018
+ */
+
 package comp1451.lab08b.driver;
 
 public class CompanyDriver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
