327cd9a51fe07fe8cf26eaf96721852bb2be27ff
Martin Zhang <mtnz6748@gmail.com>
2018-11-12 18:12:15 -0800

    get constructor, getters, setters

--- src/comp1451/lab08b/data/SalariedEmployee.java
+++ src/comp1451/lab08b/data/SalariedEmployee.java
@@ -1,8 +1,41 @@
 package comp1451.lab08b.data;
 
-public class SalariedEmployee {
+public class SalariedEmployee extends Employee{
 	
 	private double yearlySalary;
 	private int exceptedWeeksWorkPerYear;
+	
+	public SalariedEmployee() {
+		super();
+	}
+	
+	public SalariedEmployee(double yearlySalary, int exceptedWeeksWorkPerYear) {
+		super();
+		this.yearlySalary = yearlySalary;
+		this.exceptedWeeksWorkPerYear = exceptedWeeksWorkPerYear;
+	}
+
+	public double getYearlySalary() {
+		return yearlySalary;
+	}
+
+	public void setYearlySalary(double yearlySalary) {
+		this.yearlySalary = yearlySalary;
+	}
+
+	public int getExceptedWeeksWorkPerYear() {
+		return exceptedWeeksWorkPerYear;
+	}
+
+	public void setExceptedWeeksWorkPerYear(int exceptedWeeksWorkPerYear) {
+		this.exceptedWeeksWorkPerYear = exceptedWeeksWorkPerYear;
+	}
+
+	@Override
+	public String toString() {
+		return "SalariedEmployee [yearlySalary=" + yearlySalary + ", exceptedWeeksWorkPerYear="
+				+ exceptedWeeksWorkPerYear + "]";
+	}
+	
 	
 }
