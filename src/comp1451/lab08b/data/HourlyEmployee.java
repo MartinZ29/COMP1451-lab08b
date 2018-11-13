532a330b02046dd18fb909938fcb6c2e515f2cc5
Martin Zhang <mtnz6748@gmail.com>
2018-11-12 18:22:37 -0800

    add private fields, constructors, getters, setters and method to class

--- src/comp1451/lab08b/data/HourlyEmployee.java
+++ src/comp1451/lab08b/data/HourlyEmployee.java
@@ -1,5 +1,33 @@
 package comp1451.lab08b.data;
 
-public class HourlyEmployee {
+public class HourlyEmployee extends Employee{
 
+	private int hoursWorked;
+	private double hourlyWage;
+	
+	public HourlyEmployee() {
+		super(); 
+	}
+	
+	public HourlyEmployee(String firstName, String lastName, int hoursWorked) {
+		super(firstName, lastName);
+		this.hoursWorked = hoursWorked;
+	}
+	
+	public int getHoursWorked() {
+		return hoursWorked;
+	}
+	
+	public void setHoursWorked(int hoursWorked) {
+		this.hoursWorked = hoursWorked;
+	}
+	
+	@Override
+	public String toString() {
+		return "HourlyEmployee [hoursWorked=" + hoursWorked + ", HOURLY_WAGE=" + HOURLY_WAGE + "]";
+	}
+	
+	public double calculateMonthlyEarnings(){
+		return hoursWorked*HOURLY_WAGE;
+	}
 }
