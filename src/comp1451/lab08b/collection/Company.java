6c08e6f48b9ef011c73af4107e182b92e10f0147
Martin Zhang <mtnz6748@gmail.com>
2018-11-18 11:58:20 -0800

    displayAllEmployee method to get names and monthlyEarnings

--- src/comp1451/lab08b/collection/Company.java
+++ src/comp1451/lab08b/collection/Company.java
@@ -36,6 +36,12 @@ public class Company {
 	}
 	
 	public void displayAllEmployee() {
-		
+		if(employees != null) {
+			for(Employee e : employees) {
+				System.out.println("Employee [firstName = " + e.getFirstName() + ", lastName= " + e.getLastName() + " , monthlyEarnings = " + e.calculateMonthlyEarnings());
+			}
+		}else {
+			System.out.println("The company has no employee.");
+		}
 	}
 }
