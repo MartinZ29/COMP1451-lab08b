d54b23ba4a28205672cc3d22283402b20e4b1292
Martin Zhang <mtnz6748@gmail.com>
2018-11-18 13:38:55 -0800

    add java doc and fix spelling error

--- src/comp1451/lab08b/data/SalariedEmployee.java
+++ src/comp1451/lab08b/data/SalariedEmployee.java
@@ -10,41 +10,76 @@ package comp1451.lab08b.data;
 public class SalariedEmployee extends Employee{
 	
 	private double yearlySalary;
-	private int exceptedWeeksWorkPerYear;
+	private int expectedWeeksWorkPerYear;
 	
+	/**
+	 * Default constructor to inherit from abstract class
+	 */
 	public SalariedEmployee() {
 		super();
 	}
 	
-	public SalariedEmployee(String firstName, String lastName, double yearlySalary, int exceptedWeeksWorkPerYear) {
+	/**
+	 * Overload constructor which has four parameters to the class
+	 * @param firstName
+	 * 				- is the first name of the employee
+	 * @param lastName
+	 * 				- is the last name of the employee
+	 * @param yearlySalary
+	 * 				- is the yearly salary of the employee
+	 * @param expectedWeeksWorkPerYear
+	 * 				- is the expected weeks worked per year for the employee
+	 */
+	public SalariedEmployee(String firstName, String lastName, double yearlySalary, int expectedWeeksWorkPerYear) {
 		super(firstName, lastName);
 		setYearlySalary(yearlySalary);
-		setExceptedWeeksWorkPerYear(exceptedWeeksWorkPerYear);
+		setExpectedWeeksWorkPerYear(expectedWeeksWorkPerYear);
 	}
-
+	
+	/**
+	 * Accessor of yearlySalary
+	 * @return yearlySalary
+	 */
 	public double getYearlySalary() {
 		return yearlySalary;
 	}
 
+	/**
+	 * Mutator of yearlySalary
+	 * @param yearlySalary
+	 * 				- - is the yearly salary of the employee
+	 */
 	public void setYearlySalary(double yearlySalary) {
 		this.yearlySalary = yearlySalary;
 	}
 
-	public int getExceptedWeeksWorkPerYear() {
-		return exceptedWeeksWorkPerYear;
+	/**
+	 * Accessor of expectedWeeksWorkPerYear
+	 * @return expectedWeekWorkPerYear
+	 */
+	public int getExpectedWeeksWorkPerYear() {
+		return expectedWeeksWorkPerYear;
 	}
 
-	public void setExceptedWeeksWorkPerYear(int exceptedWeeksWorkPerYear) {
-		this.exceptedWeeksWorkPerYear = exceptedWeeksWorkPerYear;
+	/**
+	 * Mutator of expectedWeeksWorkPerYear
+	 * @param expectedWeeksWorkPerYear
+	 * 								- is the expected weeks worked per year for the employee
+	 */
+	public void setExpectedWeeksWorkPerYear(int expectedWeeksWorkPerYear) {
+		this.expectedWeeksWorkPerYear = expectedWeeksWorkPerYear;
 	}
 
 	@Override
 	public String toString() {
-		return "SalariedEmployee [yearlySalary=" + yearlySalary + ", exceptedWeeksWorkPerYear="
-				+ exceptedWeeksWorkPerYear + "]";
+		return "SalariedEmployee [yearlySalary=" + yearlySalary + ", expectedWeeksWorkPerYear="
+				+ expectedWeeksWorkPerYear + "]";
 	}
 	
+	/**
+	 * method inherits from abstract class to calculate monthly earnings for SalariedEmployee
+	 */
 	public double calculateMonthlyEarnings() {
-		return yearlySalary/exceptedWeeksWorkPerYear*(52/12);
+		return yearlySalary/expectedWeeksWorkPerYear*(52/12);
 	}
 }
