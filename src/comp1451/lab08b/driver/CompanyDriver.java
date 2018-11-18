b3e7b26f073559f62509af8b7c70777e844a5118
Martin Zhang <mtnz6748@gmail.com>
2018-11-18 12:01:07 -0800

    import files to driver class

--- src/comp1451/lab08b/driver/CompanyDriver.java
+++ src/comp1451/lab08b/driver/CompanyDriver.java
@@ -7,10 +7,15 @@
 
 package comp1451.lab08b.driver;
 
+import java.util.ArrayList;
+import comp1451.lab08b.collection.Company;
+import comp1451.lab08b.data.*;
+
 public class CompanyDriver {
 
 	public static void main(String[] args) {
 		
+		ArrayList<Employee> employees = new ArrayList<Employee>();
 
 	}
 
