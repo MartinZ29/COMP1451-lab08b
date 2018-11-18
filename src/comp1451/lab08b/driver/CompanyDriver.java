2b2db8c649f8315c5df33f81867b431ce2c105b2
Martin Zhang <mtnz6748@gmail.com>
2018-11-18 13:25:07 -0800

    add comments

--- src/comp1451/lab08b/driver/CompanyDriver.java
+++ src/comp1451/lab08b/driver/CompanyDriver.java
@@ -16,7 +16,11 @@ public class CompanyDriver {
 	public static void main(String[] args) {
 		
 		Company employees = new Company();
+		
+		//test empty company error msg;
 		employees.displayAllEmployee();
+		
+		//add three types of employees to company;
 		HourlyEmployee employeeOne = new HourlyEmployee("abc","def",40,50);
 		CommissionEmployee employeeTwo = new CommissionEmployee("ghi","hjk",0.5,8000);
 		SalariedEmployee employeeThree = new SalariedEmployee("lmn","opq",50000,11);
@@ -24,6 +28,8 @@ public class CompanyDriver {
 		employees.addEmployee(employeeTwo);
 		employees.addEmployee(employeeThree);
 		employees.displayAllEmployee();
+		
+		//remove one employee from company
 		employees.removeEmpolyee(employeeThree);
 		employees.displayAllEmployee();
 	}
